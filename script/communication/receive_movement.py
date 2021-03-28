da1031e8fc1463190b361e8c626ca498a695573f
laurentaubin <44675437+laurentaubin@users.noreply.github.com>
2021-03-28 12:06:18 -0400

    Change receive path script to receive movement (#42)

--- script/communication/receive_movement.py
+++ script/communication/receive_movement.py
@@ -6,6 +6,6 @@ context = zmq.Context()
 receiver_socket = context.socket(zmq.REP)
 receiver_socket.connect(SOCKET_LOCAL_BASE_ADDRESS + GAME_CYCLE_PORT)
 
-path = receiver_socket.recv_pyobj()
+movements = receiver_socket.recv_pyobj()
 
-print(path)
+print(movements[0].get_distance().get_distance())
