c63328582447cca42ab38053665fb8fb25a75470
Toma Gagne <46379340+TomaGagne@users.noreply.github.com>
2021-04-05 08:28:26 -0400

    [Script] Take image with embedded camera (#45)

---
+++ script/camera/EmbeddedCameraTakeImageScript.py
@@ -0,0 +1,56 @@
+import cv2
+
+CAMERA_INDEX = 0
+EMBEDDED_CAMERA_IMAGE_SIZE = (640, 480)
+
+
+class OpenCvEmbeddedCamera:
+    def __init__(
+        self,
+        camera_index: int,
+    ):
+        self._camera_index = camera_index
+        self._capture = None
+        self._open_capture()
+
+    def take_image(self):
+        return self._get_camera_frame()
+
+    def _get_camera_frame(self):
+        opened_successfully, current_frame = self._capture.read()
+        return current_frame
+
+    def _open_capture(self):
+        self._capture = cv2.VideoCapture(self._camera_index)
+        self._capture.set(cv2.CAP_PROP_FRAME_WIDTH, EMBEDDED_CAMERA_IMAGE_SIZE[0])
+        self._capture.set(cv2.CAP_PROP_FRAME_HEIGHT, EMBEDDED_CAMERA_IMAGE_SIZE[1])
+        self._capture.set(cv2.CAP_PROP_BUFFERSIZE, 1)
+
+    def _close_capture(self):
+        self._capture.release()
+
+
+if __name__ == "__main__":
+    camera = OpenCvEmbeddedCamera(CAMERA_INDEX)
+    should_continue = True
+    images = []
+
+    while should_continue:
+        image = camera.take_image()
+        cv2.imshow("image", image)
+        k = cv2.waitKey(33)
+        if k == 27:  # Esc key to stop
+            break
+        elif k == 32:
+            images.append(image)
+        else:
+            continue
+        cv2.destroyAllWindows()
+    cv2.destroyAllWindows()
+
+    filename = "corners/image-{}.jpg"
+
+    for index, image in enumerate(images):
+        cv2.imwrite(filename.format(index), image)
+
+    print("Script done ...")
