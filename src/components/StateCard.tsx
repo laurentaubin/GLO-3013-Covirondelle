c29128969516e3821b3abcd4959d58e750542c04
laurentaubin <44675437+laurentaubin@users.noreply.github.com>
2021-04-21 14:39:35 -0400

    Pause timer between each game cycles (#17)

--- src/components/StateCard.tsx
+++ src/components/StateCard.tsx
@@ -1,100 +1,100 @@
-import {Grid, Paper, Theme} from "@material-ui/core";
-import {createStyles, WithStyles, withStyles} from "@material-ui/core/styles";
-import React, {Component} from "react";
+import { Grid, Paper, Theme } from "@material-ui/core";
+import { createStyles, WithStyles, withStyles } from "@material-ui/core/styles";
+import React, { Component } from "react";
 import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
 
 const styles = (theme: Theme) =>
-    createStyles({
-        root: {
-            flex: 1,
-            margin: 20,
-            minWidth: 300,
-            maxWidth: 800,
-            color: "#000000"
-        },
-        active: {
-            flex: 1,
-            color: "#ffffff",
-            paddingTop: 8,
-            paddingBottom: 8,
-            padding: 20,
-            backgroundColor: theme.palette.primary.light,
-            margin: 10
-        },
-        disabled: {
-            flex: 1,
-            color: "#F0F0F0",
-            paddingTop: 8,
-            paddingBottom: 8,
-            padding: 20,
-            backgroundColor: "#D3D3D3",
-            margin: 10
-        },
-        actionButton: {
-            flex: 1,
-            padding: 20,
-            margin: 5,
-            backgroundColor: theme.palette.success.main,
-            color: "#ffffff",
-            borderRadius: 100
-        },
-        disabledButton: {
-            opacity: 0.7
-        }
-    });
+  createStyles({
+    root: {
+      flex: 1,
+      margin: 20,
+      minWidth: 300,
+      maxWidth: 800,
+      color: "#000000",
+    },
+    active: {
+      flex: 1,
+      color: "#ffffff",
+      paddingTop: 8,
+      paddingBottom: 8,
+      padding: 20,
+      backgroundColor: theme.palette.primary.light,
+      margin: 10,
+    },
+    disabled: {
+      flex: 1,
+      color: "#F0F0F0",
+      paddingTop: 8,
+      paddingBottom: 8,
+      padding: 20,
+      backgroundColor: "#D3D3D3",
+      margin: 10,
+    },
+    actionButton: {
+      flex: 1,
+      padding: 20,
+      margin: 5,
+      backgroundColor: theme.palette.success.main,
+      color: "#ffffff",
+      borderRadius: 100,
+    },
+    disabledButton: {
+      opacity: 0.7,
+    },
+  });
 
 type Props = {
-    title: string;
-    active: string;
-    neutral: string;
-    isActive: boolean;
-    isWaiting: boolean;
-    onClick?: () => void;
-    label?: string;
+  title: string;
+  active: string;
+  neutral: string;
+  isActive: boolean;
+  isWaiting: boolean;
+  onClick?: () => void;
+  label?: string;
 };
 
 type AllProps = Props & WithStyles<typeof styles>;
 
 class StateCard extends Component<AllProps> {
-    public render() {
-        const {classes} = this.props;
-        const {title, active, neutral, isActive, isWaiting, onClick, label} = this.props;
+  public render() {
+    const { classes } = this.props;
+    const { title, active, neutral, isActive, isWaiting, onClick, label } = this.props;
 
-        return (
-            <div className={classes.root} data-testid={"state-card"}>
-                <Typography gutterBottom variant="h5" component="h2">
-                    {title}
-                </Typography>
-                <Grid container spacing={0}>
-                    <Grid item xs={6}>
-                        <Paper className={isActive ? classes.active : classes.disabled}>
-                            <Typography variant="body2" component="p">
-                                {active}
-                            </Typography>
-                        </Paper>
-                    </Grid>
-                    <Grid item xs={6}>
-                        <Paper className={isActive ? classes.disabled : classes.active}>
-                            <Typography variant="body2" component="p">
-                                {neutral}
-                            </Typography>
-                        </Paper>
-                    </Grid>
-                </Grid>
-                {onClick && (
-                    <Button
-                        className={classes.actionButton}
-                        onClick={onClick}
-                        disabled={isActive || isWaiting}
-                        classes={{disabled: classes.disabledButton}}
-                    >
-                        {label ?? "DÉMARRER"}
-                    </Button>
-                )}
-            </div>
-        );
-    }
+    return (
+      <div className={classes.root} data-testid={"state-card"}>
+        <Typography gutterBottom variant="h5" component="h2">
+          {title}
+        </Typography>
+        <Grid container spacing={0}>
+          <Grid item xs={6}>
+            <Paper className={isActive ? classes.active : classes.disabled}>
+              <Typography variant="body2" component="p">
+                {active}
+              </Typography>
+            </Paper>
+          </Grid>
+          <Grid item xs={6}>
+            <Paper className={isActive ? classes.disabled : classes.active}>
+              <Typography variant="body2" component="p">
+                {neutral}
+              </Typography>
+            </Paper>
+          </Grid>
+        </Grid>
+        {onClick && (
+          <Button
+            className={classes.actionButton}
+            onClick={onClick}
+            disabled={isActive || isWaiting}
+            classes={{ disabled: classes.disabledButton }}
+          >
+            {label ?? "DÉMARRER"}
+          </Button>
+        )}
+      </div>
+    );
+  }
 }
 
 const componentWithStyles = withStyles(styles)(StateCard);
--- src/components/StopWatch.tsx
+++ src/components/StopWatch.tsx
@@ -1,96 +1,90 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from "react";
 import Typography from "@material-ui/core/Typography";
-import {Grid, Paper, Theme} from "@material-ui/core";
-import {createStyles, withStyles} from "@material-ui/core/styles";
+import { Button, Grid, Paper, Theme } from "@material-ui/core";
+import { createStyles, withStyles } from "@material-ui/core/styles";
 
-const styles = () => createStyles({
+const styles = () =>
+  createStyles({
     root: {
-        margin: 20,
-        color: "#000000"
+      margin: 20,
+      color: "#000000",
     },
     data: {
-        flex:1,
-        margin: 2,
-        paddingTop: 12,
-        paddingBottom: 12,
-        padding: 4
+      flex: 1,
+      margin: 2,
+      paddingTop: 12,
+      paddingBottom: 12,
+      padding: 4,
+    },
+  });
+
+var interval: any = 0;
+
+const StopWatch = (props: any) => {
+  const { isStarted, isEnded, classes } = props;
+  const [millisecond, setMillisecond] = useState(0);
+  const [second, setSecond] = useState(0);
+  const [minute, setMinute] = useState(0);
+  useEffect(() => {
+    if (isStarted && !isEnded) {
+      interval = setInterval(() => {
+        setMillisecond((millisecond) => millisecond + 1);
+        if (millisecond === 100) {
+          setSecond((second) => second + 1);
+          setMillisecond(0);
+        }
+        if (second === 60) {
+          setMinute((minute) => minute + 1);
+          setSecond(0);
+        }
+      }, 10);
     }
-});
 
-var   interval: any = 0;
+    return () => clearInterval(interval);
+  }, [second, millisecond, isStarted, isEnded]);
 
-const StopWatch = (props: any) =>  {
-    const {isStarted, isEnded, classes} = props
-    const [millisecond, setMillisecond] = useState(0)
-    const [second, setSecond] = useState(0)
-    const [minute, setMinute] = useState(0)
-    useEffect(()=>{
-        if(isStarted && !isEnded){
-            interval = setInterval(()=>{
-
-                setMillisecond(millisecond => millisecond +1)
-                if(millisecond === 100){
-                    setSecond(second => second + 1)
-                    setMillisecond(0)
-                }
-                if(second === 60){
-                    setMinute(minute => minute +1)
-                    setSecond(0)
-                }
-
-            },10);
-
-        }
-        else {
-            clearInterval(interval);
-        }
-
-        return () => clearInterval(interval);
-     }, [second, millisecond, isStarted, isEnded]);
-
-
-    return(
-        <div className={classes.root}>
-            <Grid container spacing={3}>
-                <Grid item xs={12}>
-                    <Typography gutterBottom variant="h5" component="h2">
-                        Temps écoulé
-                    </Typography>
-                </Grid>
-                <Grid item xs={12} sm={4}>
-                    <Paper className={classes.data}>
-                        <Typography  variant="body2" component="p">
-                            {minute}
-                        </Typography>
-                        <Typography  variant="body2" component="p">
-                            m
-                        </Typography>
-                    </Paper>
-                </Grid>
-                <Grid item xs={12} sm={4}>
-                    <Paper className={classes.data}>
-                        <Typography  variant="body2" component="p">
-                            {second}
-                        </Typography>
-                        <Typography  variant="body2" component="p">
-                            s
-                        </Typography>
-                    </Paper>
-                </Grid>
-                <Grid item xs={12} sm={4}>
-                    <Paper className={classes.data}>
-                        <Typography  variant="body2" component="p">
-                            {millisecond}
-                        </Typography>
-                        <Typography  variant="body2" component="p">
-                            ms
-                        </Typography>
-                    </Paper>
-                </Grid>
-            </Grid>
-        </div>
-    );
-}
+  return (
+    <div className={classes.root}>
+      <Grid container spacing={3}>
+        <Grid item xs={12}>
+          <Typography gutterBottom variant="h5" component="h2">
+            Temps écoulé
+          </Typography>
+        </Grid>
+        <Grid item xs={12} sm={4}>
+          <Paper className={classes.data}>
+            <Typography variant="body2" component="p">
+              {minute}
+            </Typography>
+            <Typography variant="body2" component="p">
+              m
+            </Typography>
+          </Paper>
+        </Grid>
+        <Grid item xs={12} sm={4}>
+          <Paper className={classes.data}>
+            <Typography variant="body2" component="p">
+              {second}
+            </Typography>
+            <Typography variant="body2" component="p">
+              s
+            </Typography>
+          </Paper>
+        </Grid>
+        <Grid item xs={12} sm={4}>
+          <Paper className={classes.data}>
+            <Typography variant="body2" component="p">
+              {millisecond}
+            </Typography>
+            <Typography variant="body2" component="p">
+              ms
+            </Typography>
+          </Paper>
+        </Grid>
+      </Grid>
+    </div>
+  );
+};
 
 const componentWithStyles = withStyles(styles)(StopWatch);
 export default componentWithStyles;
