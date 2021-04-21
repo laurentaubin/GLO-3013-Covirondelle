117152131fcd6320ae10141af76320274f9b2085
Toma Gagne <46379340+TomaGagne@users.noreply.github.com>
2021-04-21 08:32:10 -0400

    Fix path drawing (#18)

--- src/components/TableDisplay.tsx
+++ src/components/TableDisplay.tsx
@@ -5,7 +5,6 @@ import {AppContext} from "../context/context";
 const TableDisplay = () => {
     const PLANNED_TRAJECTORY_COLOR = '#0000FF'
     const ROBOT_REAL_PATH_COLOR = '#ff0000'
-    const IMAGE_RATIO = 2.5
     const {state} = useContext(AppContext);
     const [image, setImage] = useState(null)
     const canvas = useRef(null)
@@ -22,7 +21,7 @@ const TableDisplay = () => {
         if (image && canvas) {
             // @ts-ignore
             const context = canvas.current.getContext("2d");
-            context.drawImage(image, 0, 0, 640, 480);
+            context.drawImage(image, 0, 0, 1280, 800);
         }
     }, [image, canvas])
 
@@ -30,8 +29,8 @@ const TableDisplay = () => {
         // @ts-ignore
         const context = canvas.current.getContext("2d");
         if (context !== null) {
-            const x_current_position = state.RobotPosition?.x_coordinate ?? 0 / IMAGE_RATIO
-            const y_current_position = state.RobotPosition?.y_coordinate ?? 0 / IMAGE_RATIO
+            const x_current_position = state.RobotPosition?.x_coordinate ?? 0
+            const y_current_position = state.RobotPosition?.y_coordinate ?? 0
             context.beginPath()
             if (lastRobotPosition.x_coordinate == 0 && lastRobotPosition.y_coordinate == 0) {
                 context.moveTo(x_current_position, y_current_position)
@@ -57,8 +56,8 @@ const TableDisplay = () => {
                 let lastPosition = state.currentPlannedTrajectory[0]
                 context.strokeStyle = PLANNED_TRAJECTORY_COLOR;
                 state.currentPlannedTrajectory.map(position => {
-                    context.moveTo(lastPosition.x_coordinate / IMAGE_RATIO, lastPosition.y_coordinate / IMAGE_RATIO)
-                    context.lineTo(position.x_coordinate / IMAGE_RATIO, position.y_coordinate / IMAGE_RATIO)
+                    context.moveTo(lastPosition.x_coordinate, lastPosition.y_coordinate)
+                    context.lineTo(position.x_coordinate, position.y_coordinate)
                     context.stroke()
                     lastPosition = position
                 })
@@ -68,7 +67,7 @@ const TableDisplay = () => {
 
     return (
         <div>
-            <canvas ref={canvas} width={640} height={480}/>
+            <canvas ref={canvas} width={1280} height={800}/>
         </div>
     )
 }
